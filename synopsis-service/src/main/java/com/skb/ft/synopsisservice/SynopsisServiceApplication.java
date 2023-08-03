8022aafda8e6fbecabd7facd231ed8f205e747b8
prkim99@gmail.com <prkim99@gmail.com>
2023-08-03 23:19:15 +0900

    initial commit

---
+++ synopsis-service/src/main/java/com/skb/ft/synopsisservice/SynopsisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skb.ft.synopsisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynopsisServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynopsisServiceApplication.class, args);
+    }
+
+}
---
+++ synopsis-service/src/test/java/com/skb/ft/synopsisservice/SynopsisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skb.ft.synopsisservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynopsisServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
