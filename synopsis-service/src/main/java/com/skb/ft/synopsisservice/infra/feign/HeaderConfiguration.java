cc74cec0dfce597c4521de7295fbdf776caeed2c
prkim99_mac <prkim99@gmail.com>
2023-08-04 17:29:16 +0900

    [feat] feign: 공통 샘플 헤더 config 등록

---
+++ synopsis-service/src/main/java/com/skb/ft/synopsisservice/infra/feign/HeaderConfiguration.java
@@ -0,0 +1,33 @@
+package com.skb.ft.synopsisservice.infra.feign;
+
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+public class HeaderConfiguration {
+    @Value("env.AUTH_VAL")
+    String authVal;
+    @Value("env.CLIENTID")
+    String clientId;
+    @Value("env.TIMESTAMP")
+    String timestamp;
+    @Value("env.UUID")
+    String uuid;
+    @Value("env.TRACE")
+    String trace;
+    @Value("env.API_KEY")
+    String apiKey;
+    @Bean
+    public RequestInterceptor requestInterceptor(){
+        return requestTemplate -> {
+            requestTemplate.header("Auth_val",authVal);
+            requestTemplate.header("Client_ID",clientId);
+            requestTemplate.header("Timestamp",timestamp);
+            requestTemplate.header("UUID",uuid);
+            requestTemplate.header("Timestamp",timestamp);
+            requestTemplate.header("Api_Key",apiKey);
+        };
+    }
+}
