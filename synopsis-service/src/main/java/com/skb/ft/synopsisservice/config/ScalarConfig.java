72bd9c4416e3355a120875a502bf11ecb69423da
prkim99_mac <prkim99@gmail.com>
2023-08-08 01:22:37 +0900

    [feat] AliasScalar(DATE)와 ExtendedScalar(JSON) 등록

---
+++ synopsis-service/src/main/java/com/skb/ft/synopsisservice/config/ScalarConfig.java
@@ -0,0 +1,19 @@
+package com.skb.ft.synopsisservice.config;
+
+import graphql.Scalars;
+import graphql.scalars.ExtendedScalars;
+import graphql.scalars.alias.AliasedScalar;
+import graphql.schema.GraphQLScalarType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class ScalarConfig {
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        GraphQLScalarType Date = ExtendedScalars.newAliasedScalar("Date").aliasedScalar(Scalars.GraphQLString).build();
+        return wiringBuilder -> wiringBuilder.scalar(Date).scalar(ExtendedScalars.Json);
+    }
+
+}
