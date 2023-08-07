3cf936f2e6f90655e2907c676e45bc08c08b9db5
prkim99_mac <prkim99@gmail.com>
2023-08-08 01:24:56 +0900

    [feat] euxp: queryMapping 기능 작성

---
+++ synopsis-service/src/main/java/com/skb/ft/synopsisservice/Controller.java
@@ -0,0 +1,22 @@
+package com.skb.ft.synopsisservice;
+
+import com.skb.ft.synopsisservice.domain.euxp.EuxpService;
+import com.skb.ft.synopsisservice.domain.euxp.client.EuxpRequestParam;
+import com.skb.ft.synopsisservice.domain.euxp.vo.EuxpSynopsisResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Arguments;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+
+import java.util.Map;
+
+@org.springframework.stereotype.Controller
+@RequiredArgsConstructor
+public class Controller {
+    private final EuxpService euxpService;
+    @QueryMapping
+    public EuxpSynopsisResponseDto euxpQuery(@Arguments EuxpRequestParam euxpRequestParam){
+        EuxpSynopsisResponseDto  euxpSynopsisResponseDto = euxpService.callEuxpResponse(euxpRequestParam);
+        return euxpSynopsisResponseDto;
+    }
+
+}
