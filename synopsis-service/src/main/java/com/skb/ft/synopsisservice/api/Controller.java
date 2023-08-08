b881bc75576b8b0bf5f2841d419a94ba3ba18349
prkim99@gmail.com <prkim99@gmail.com>
2023-08-09 01:49:35 +0900

    [feat] smd: controller 내 좋아요 정보 요청 함수 추가

--- synopsis-service/src/main/java/com/skb/ft/synopsisservice/api/Controller.java
+++ synopsis-service/src/main/java/com/skb/ft/synopsisservice/api/Controller.java
@@ -3,6 +3,9 @@ package com.skb.ft.synopsisservice.api;
 import com.skb.ft.synopsisservice.domain.euxp.EuxpService;
 import com.skb.ft.synopsisservice.domain.euxp.client.EuxpRequestParam;
 import com.skb.ft.synopsisservice.domain.euxp.vo.EuxpSynopsisResponseDto;
+import com.skb.ft.synopsisservice.domain.smd.SmdService;
+import com.skb.ft.synopsisservice.domain.smd.client.SmdRequestParam;
+import com.skb.ft.synopsisservice.domain.smd.vo.SmdLikeHateResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Arguments;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -13,10 +16,16 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class Controller {
     private final EuxpService euxpService;
+    private final SmdService smdService;
     @QueryMapping
     public EuxpSynopsisResponseDto euxpQuery(@Arguments EuxpRequestParam euxpRequestParam){
         EuxpSynopsisResponseDto  euxpSynopsisResponseDto = euxpService.callEuxpResponse(euxpRequestParam);
         return euxpSynopsisResponseDto;
+    }
+    @QueryMapping
+    public SmdLikeHateResponseDto smdGetLikeHateQuery(@Arguments SmdRequestParam smdRequestParam){
+        SmdLikeHateResponseDto smdLikeHateResponseDto = smdService.callSmdLikeHateResponse(smdRequestParam);
+        return smdLikeHateResponseDto;
     }
 
 }
