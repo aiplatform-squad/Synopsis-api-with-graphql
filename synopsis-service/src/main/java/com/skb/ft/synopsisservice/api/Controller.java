7192df016efe1d4d34ba73c9556ab385090fd764
prkim99_mac <prkim99@gmail.com>
2023-08-09 14:16:44 +0900

    [feat] scs: controller 생성

--- synopsis-service/src/main/java/com/skb/ft/synopsisservice/api/Controller.java
+++ synopsis-service/src/main/java/com/skb/ft/synopsisservice/api/Controller.java
@@ -3,20 +3,27 @@ package com.skb.ft.synopsisservice.api;
 import com.skb.ft.synopsisservice.domain.euxp.EuxpService;
 import com.skb.ft.synopsisservice.domain.euxp.client.EuxpRequestParam;
 import com.skb.ft.synopsisservice.domain.euxp.vo.EuxpSynopsisResponseDto;
+import com.skb.ft.synopsisservice.domain.scs.ScsService;
+import com.skb.ft.synopsisservice.domain.scs.dto.ScsDirectviewRequestDto;
+import com.skb.ft.synopsisservice.domain.scs.dto.ScsDirectviewResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Arguments;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-
-import java.util.Map;
 
 @org.springframework.stereotype.Controller
 @RequiredArgsConstructor
 public class Controller {
     private final EuxpService euxpService;
+    private final ScsService scsService;
     @QueryMapping
     public EuxpSynopsisResponseDto euxpQuery(@Arguments EuxpRequestParam euxpRequestParam){
         EuxpSynopsisResponseDto  euxpSynopsisResponseDto = euxpService.callEuxpResponse(euxpRequestParam);
         return euxpSynopsisResponseDto;
+    }
+    @QueryMapping
+    public ScsDirectviewResponseDto scsDirectviewQuery(@Arguments ScsDirectviewRequestDto scsDirectviewRequestDto){
+        ScsDirectviewResponseDto scsDirectviewResponseDto=scsService.callScsDirectviewResponse(scsDirectviewRequestDto);
+        return  scsDirectviewResponseDto;
     }
 
 }
