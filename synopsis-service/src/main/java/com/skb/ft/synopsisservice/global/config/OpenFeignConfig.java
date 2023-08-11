08e47005c417bfae3540685c7c47ca89d006f12b
prkim99_mac <prkim99@gmail.com>
2023-08-11 22:05:11 +0900

    [feat] feign: errorDecoder기반 응답 에러 코드에 따른 retryer 수행 기능 작성

--- synopsis-service/src/main/java/com/skb/ft/synopsisservice/global/config/OpenFeignConfig.java
+++ synopsis-service/src/main/java/com/skb/ft/synopsisservice/global/config/OpenFeignConfig.java
@@ -1,8 +1,16 @@
 package com.skb.ft.synopsisservice.global.config;
 
 import feign.Logger;
+import feign.RetryableException;
+import feign.Retryer;
+import feign.codec.ErrorDecoder;
+import graphql.execution.AbortExecutionException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import static java.lang.String.format;
 
 @Configuration
 public class OpenFeignConfig {
@@ -14,6 +22,27 @@ public class OpenFeignConfig {
 //        HEADERS, Log the basic information along with request and response headers.
 //        FULL, Log the headers, body, and metadata for both requests and responses
     }
+    @Bean
+    public ErrorDecoder errorDecoder(){
+        return(methodKey, response)->{
+            if(HttpStatus.Series.valueOf(response.status()) ==HttpStatus.Series.SERVER_ERROR){
+                return new RetryableException(response.status(),
+                        format("%s 요청이 성공하지 못했습니다. Retry 합니다. - status: %s, headers: %s", methodKey, response.status(), response.headers()),
+                        response.request().httpMethod(),
+                        null
+                        ,response.request());
+            } else if (HttpStatus.Series.valueOf(response.status()) ==HttpStatus.Series.CLIENT_ERROR) {
+                return new AbortExecutionException(format("%s 요청이 성공하지 못했습니다. Retry 합니다. - status: %s, headers: %s", methodKey, response.status(), response.headers()));
+            }
+            return new IllegalStateException(format("%s 요청이 성공하지 못했습니다. - status: %s, headers: %s", methodKey, response.status(), response.headers()));
+        };
+    }
 
+    @Bean
+    public Retryer retryer(){
+        // period: 실행 주기, maxPeriod: interval 이 더 클 경우 반환 maxAttempts: 최대 몇번 실행
+        //1초 간격으로 최대 3회까지 재시도. 최대 재시도 간격은 2초
+        return new Retryer.Default(1000,2000,3);
+    }
 
 }
